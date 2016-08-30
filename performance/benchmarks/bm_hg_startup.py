640d11306ace3080e707d6f6bc7765052e3a96e2
Victor Stinner <vstinner@redhat.com>
2016-08-30 03:01:07 +0200

    bm_hg_startup: add hg version metadata

--- performance/benchmarks/bm_hg_startup.py
+++ performance/benchmarks/bm_hg_startup.py
@@ -7,6 +7,25 @@ from performance.venv import get_venv_program
 from six.moves import xrange
 
 
+def get_hg_version(hg_bin):
+    # Fast-path: use directly the Python module
+    try:
+        from mercurial.__version__ import version
+        return version
+    except ImportError:
+        pass
+
+    # Slow-path: run the "hg --version" command
+    proc = subprocess.Popen([sys.executable, hg_bin, "--version"],
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+    stdout = proc.communicate()[0]
+    if proc.returncode:
+        print("ERROR: Mercurial command failed!")
+        sys.exit(proc.returncode)
+    return stdout.splitlines()[0]
+
+
 def run(command):
     subprocess.Popen
 
@@ -37,8 +56,7 @@ if __name__ == "__main__":
     args = runner.parse_args()
 
     hg_bin = get_venv_program('hg')
+    runner.metadata['hg_version'] = get_hg_version(hg_bin)
 
     command = [sys.executable, hg_bin, "help"]
-    # FIXME: add mercurial version to metadata
-
     runner.bench_sample_func(bench_startup, command)
