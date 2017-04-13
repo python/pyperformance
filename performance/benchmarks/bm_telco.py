cdfcc87b271f181057326670c759ef8ea38f1470
Victor Stinner <victor.stinner@gmail.com>
2017-04-13 17:04:48 +0200

    Update telco URLs, fix also a typo

--- performance/benchmarks/bm_telco.py
+++ performance/benchmarks/bm_telco.py
@@ -1,11 +1,11 @@
 # coding: UTF-8
 """ Telco Benchmark for measuring the performance of decimal calculations
 
-http://www2.hursley.ibm.com/decimal/telco.html
-http://www2.hursley.ibm.com/decimal/telcoSpec.html
+- http://speleotrove.com/decimal/telco.html
+- http://speleotrove.com/decimal/telcoSpec.html
 
 A call type indicator, c, is set from the bottom (least significant) bit of the duration (hence c is 0 or 1).
-A r, r, is determined from the call type. Those calls with c=0 have a low r: 0.0013; the remainder (‘distance calls’) have a ‘premium’ r: 0.00894. (The rates are, very roughly, in Euros or dollarates per second.)
+A rate, r, is determined from the call type. Those calls with c=0 have a low r: 0.0013; the remainder (‘distance calls’) have a ‘premium’ r: 0.00894. (The rates are, very roughly, in Euros or dollarates per second.)
 A price, p, for the call is then calculated (p=r*n). This is rounded to exactly 2 fractional digits using round-half-even (Banker’s round to nearest).
 A basic tax, b, is calculated: b=p*0.0675 (6.75%). This is truncated to exactly 2 fractional digits (round-down), and the total basic tax variable is then incremented (sumB=sumB+b).
 For distance calls: a distance tax, d, is calculated: d=p*0.0341 (3.41%). This is truncated to exactly 2 fractional digits (round-down), and then the total distance tax variable is incremented (sumD=sumD+d).
