c737313798b058af17287f06ec6e7d3236ca90b4
Victor Stinner <victor.stinner@gmail.com>
2018-01-11 10:34:42 +0100

    post-release: set version to 0.6.2

--- performance/__init__.py
+++ performance/__init__.py
@@ -1,2 +1,2 @@
-VERSION = (0, 6, 1)
+VERSION = (0, 6, 2)
 __version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@
 #  - git commit -a -m "post-release"
 #  - git push
 
-VERSION = '0.6.1'
+VERSION = '0.6.2'
 
 DESCRIPTION = 'Python benchmark suite'
 CLASSIFIERS = [
