030c84ad26dd13391160e698dc18f82c08e9be5a
Victor Stinner <vstinner@redhat.com>
2019-07-31 11:30:04 +0200

    bm_2to3 now uses Runner.bench_command()

--- pyperformance/benchmarks/bm_2to3.py
+++ pyperformance/benchmarks/bm_2to3.py
@@ -1,19 +1,8 @@
 import glob
 import os.path
-import subprocess
 import sys
 
 import pyperf
-
-
-def bench_2to3(command, devnull_out):
-    proc = subprocess.Popen(command,
-                            stdout=devnull_out,
-                            stderr=devnull_out)
-    returncode = proc.wait()
-    if returncode != 0:
-        print("ERROR: 2to3 command failed!")
-        sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -26,5 +15,4 @@ if __name__ == "__main__":
     pyfiles = glob.glob(os.path.join(datadir, '*.py.txt'))
 
     command = [sys.executable, "-m", "lib2to3", "-f", "all"] + pyfiles
-    with open(os.devnull, "wb") as devnull_out:
-        runner.bench_func('2to3', bench_2to3, command, devnull_out)
+    runner.bench_command('2to3', command)
