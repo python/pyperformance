a9ebc5f7d587b6eefd3ebd4b5c3bc79072bcd4d9
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-05-14 06:25:41 +0000

    add tomllib_loads benchmark

---
+++ pyperformance/data-files/benchmarks/bm_toml_load/run_benchmark.py
@@ -0,0 +1,16 @@
+import tomllib
+import pyperf
+from pathlib import Path
+DATA_FILE = Path(__file__).parent / "data" / "data.toml"
+
+def bench_tomllib_loads(data: str, loops: int) -> float:
+    t0 = pyperf.perf_counter()
+    for _ in range(loops):
+        tomllib.loads(data)
+    return pyperf.perf_counter() - t0
+
+if __name__ == "__main__":
+    runner = pyperf.Runner()
+    runner.metadata['description'] = "Benchmark tomllib.loads()"
+    data = DATA_FILE.read_text('utf-8')
+    runner.bench_time_func('tomllib_loads', bench_tomllib_loads, data, loops=100)
