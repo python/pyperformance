1f27ef11b6ccab86934eff816a77fede1d4245ec
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-05-28 08:51:15 +0530

    Update pyperformance/data-files/benchmarks/bm_tomllib_loads/generate_data.py
    Co-authored-by: Eric Snow <ericsnowcurrently@gmail.com>

--- pyperformance/data-files/benchmarks/bm_tomllib_loads/generate_data.py
+++ pyperformance/data-files/benchmarks/bm_tomllib_loads/generate_data.py
@@ -5,16 +5,14 @@ import toml
 BASE_URL = "https://api.github.com/repos/python/cpython/pulls?per_page=1000&state=all"
 
 def main():
-    page = 1
     all_issues = []
-    while page <= 10:
+    for page in range(1, 11):
         with urlopen(f"{BASE_URL}&page={page}") as response:
             issues = json.loads(response.read())
             if not issues:
                 break
             all_issues.extend(issues)
             print(f"Page: {page} Total Issues: {len(all_issues)}")
-        page += 1
     with open("issues.toml", "w") as f:
         f.write(toml.dumps({"data": all_issues}))
 
