4ccd8db52341774f5dd194e29abafa70c4df2515
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-06-18 07:10:31 +0530

    Reduce noise in generators benchmark  (#218)
    (And fix a return type)

--- pyperformance/data-files/benchmarks/bm_generators/run_benchmark.py
+++ pyperformance/data-files/benchmarks/bm_generators/run_benchmark.py
@@ -33,12 +33,13 @@ def tree(input: range) -> Tree | None:
     i = n // 2
     return Tree(tree(input[:i]), input[i], tree(input[i + 1:]))
 
-def bench_generators(loops: int) -> None:
+def bench_generators(loops: int) -> float:
     assert list(tree(range(10))) == list(range(10))
     range_it = range(loops)
+    iterable = tree(range(100000))
     t0 = pyperf.perf_counter()
     for _ in range_it:
-        for _ in tree(range(100000)):
+        for _ in iterable:
             pass
     return pyperf.perf_counter() - t0
 
