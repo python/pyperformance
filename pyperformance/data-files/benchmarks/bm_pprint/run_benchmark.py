625f58fecd5fa808323323901f4635a8458b0830
Oleg Iarygin <oleg@arhadthedev.net>
2022-07-07 23:55:58 +0300

    Return accidentally removed full test names

--- pyperformance/data-files/benchmarks/bm_pprint/run_benchmark.py
+++ pyperformance/data-files/benchmarks/bm_pprint/run_benchmark.py
@@ -18,5 +18,6 @@ if __name__ == '__main__':
     runner.metadata['description'] = 'pprint benchmark'
 
     if hasattr(p, '_safe_repr'):
-        runner.bench_func('_safe_repr', p._safe_repr, printable, {}, None, 0)
-    runner.bench_func('pformat', p.pformat, printable)
+        runner.bench_func('pprint_safe_repr', p._safe_repr,
+                          printable, {}, None, 0)
+    runner.bench_func('pprint_pformat', p.pformat, printable)
