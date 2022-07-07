d2122a06c413686a37378912c4fa85e2dfa37914
Oleg Iarygin <oleg@arhadthedev.net>
2022-07-07 23:27:31 +0300

    Move constant object to a module level

--- pyperformance/data-files/benchmarks/bm_pprint/run_benchmark.py
+++ pyperformance/data-files/benchmarks/bm_pprint/run_benchmark.py
@@ -9,10 +9,11 @@ from pprint import PrettyPrinter
 import pyperf
 
 
-def benchmark_pprint(runner):
-    printable = [("string", (1, 2), [3, 4], {5: 6, 7: 8})] * 100_000
-    p = PrettyPrinter()
+printable = [("string", (1, 2), [3, 4], {5: 6, 7: 8})] * 100_000
+p = PrettyPrinter()
 
+
+def benchmark_pprint(runner):
     # Support CPython before 3.10 and non-CPython implementations
     if hasattr(p, '_safe_repr'):
         runner.bench_func('_safe_repr', p._safe_repr, printable, {}, None, 0)
