7aabc1374f2b69f3dc6a01850044bf099582f373
Oleg Iarygin <oleg@arhadthedev.net>
2022-07-07 23:44:29 +0300

    Make the benchmark top-level

--- pyperformance/data-files/benchmarks/bm_pprint/run_benchmark.py
+++ pyperformance/data-files/benchmarks/bm_pprint/run_benchmark.py
@@ -5,25 +5,18 @@ This benchmark was available as `python -m pprint` until Python 3.12.
 Authors: Fred Drake (original), Oleg Iarygin (pyperformance port).
 """
 
-from pprint import PrettyPrinter
 import pyperf
+from pprint import PrettyPrinter
 
 
-printable = [("string", (1, 2), [3, 4], {5: 6, 7: 8})] * 100_000
+printable = [('string', (1, 2), [3, 4], {5: 6, 7: 8})] * 100_000
 p = PrettyPrinter()
 
 
-def benchmark_pprint(runner):
-    # Support CPython before 3.10 and non-CPython implementations
-    if hasattr(p, '_safe_repr'):
-        runner.bench_func('pprint_safe_repr', p._safe_repr, printable,
-                          {}, None, 0)
-    runner.bench_func('pprint_pformat', p.pformat, printable)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     runner = pyperf.Runner()
-    runner.metadata['description'] = "pprint benchmark"
+    runner.metadata['description'] = 'pprint benchmark'
 
-    args = runner.parse_args()
-    benchmark_pprint(runner)
+    if hasattr(p, '_safe_repr'):
+        runner.bench_func('_safe_repr', p._safe_repr, printable, {}, None, 0)
+    runner.bench_func('pformat', p.pformat, printable)
