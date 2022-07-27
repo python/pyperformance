88ce6f5721a583e49ea64edde241179197be6240
Michael Droettboom <mdboom@gmail.com>
2022-07-27 11:24:26 -0400

    Allow for specifying local wheels and sdists as dependencies

---
+++ pyperformance/tests/data/bm_local_wheel/run_benchmark.py
@@ -0,0 +1,19 @@
+"""
+A dummy benchmark that uses a local wheel
+"""
+
+import pyperf
+from this_is import en_us
+
+
+def bench():
+    return 1.0
+
+
+if __name__ == "__main__":
+    runner = pyperf.Runner()
+    runner.metadata['description'] = "A dummy benchmark that has a local wheel dependency"
+
+    args = runner.parse_args()
+    runner.bench_func('local_wheel', bench)
+
